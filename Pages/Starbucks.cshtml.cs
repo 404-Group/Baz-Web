287ebb29d3c483353f259153e71929bbb2685ad2
Braden Prather <Bradenprather@gmail.com>
2023-11-07 10:16:14 -0600

    Added Images and Links to Restaurant pages

---
+++ Pages/Starbucks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCoreApp.Pages
+{
+    public class StarbucksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/WhichWich.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCoreApp.Pages
+{
+    public class WhichWichModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
