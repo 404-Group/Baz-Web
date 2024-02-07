52d05bb462585c92bde2caa392ded0aba89a8156
Braden Prather <Bradenprather@gmail.com>
2024-02-06 20:14:30 -0600

    Button Wide

--- obj/Debug/net6.0/MyCoreApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a63c3777eb77910e937ca3d637da44c38d8b86e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
